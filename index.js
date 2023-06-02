130c061e4cc64e56188d5ec199155dc4d8c5ba60
John Corn <jcornacchioli@gmail.com>
2023-06-02 19:24:55 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// code your solution here
+function saturdayFun(activity)
+{
+       if (activity == null)
+       {
+        return "This Saturday, I want to roller-skate!";
+       }
+       else
+       {
+        return `This Saturday, I want to ${activity}!`;
+       }
+}
+
+function mondayWork(activity)
+{
+    if (activity == null)
+    {
+     return "This Monday, I will go to the office.";
+    }
+    else
+    {
+     return `This Monday, I will ${activity}.`;
+    }
+}
+
+function wrapAdjective(special) 
+{
+    const part1 = "You are";
+    let adjective = "";
+
+    return function () 
+    {
+        if (special == '*')
+        {
+            adjective = "a hard worker";
+        }
+        else if (special == '||')
+        {
+            adjective = "a dedicated programmer";
+        }
+
+        return (`${part1} ${special}${adjective}${special}!`);
+    };
+  }
